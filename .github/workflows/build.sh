93a78e10e7af23e5779b3d7efa262d6133dea902
Jerrin Shaji George <jshajigeorge@vmware.com>
2020-09-03 22:38:41 +0000

    GitHub Actions
    Signed-off-by: Jerrin Shaji George <jshajigeorge@vmware.com>

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,21 +1,11 @@
-#/bin/bash
+#!/bin/bash
 
 set -e
-
-# Travis CI clones the repository at ~/vmware-samples/go-redis-pmem
-# Move the repository into GOPATH first.
-cd ..
-mkdir -p $GOPATH/src/github.com/vmware-samples
-mv go-redis-pmem $GOPATH/src/github.com/vmware-samples
 
 # Build the modified Go compiler.
 git clone --depth 1  https://github.com/jerrinsg/go-pmem ~/go-pmem
 cd ~/go-pmem/src
 ./make.bash
-
-# Force travis CI to use the compiler and toolchain we just built (TODO).
-# If the go compiler is used as ~/go-pmem/bin/go test, travis uses the tools
-# found in /home/travis/.gimme/versions/go1.11.1.linux.amd64 to do the build.
 
 # Get go-pmem-transaction repository
 GOROOT="$HOME/go-pmem/" GOTOOLDIR="$HOME/go-pmem/pkg/tool/linux_amd64" ~/go-pmem/bin/go get github.com/vmware/go-pmem-transaction/...
